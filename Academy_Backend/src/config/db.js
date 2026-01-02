6d7d9c6c9f87891d2e667484ae951532782d4b9d
Shubham Pawar <shubham.ravindra.pawar@gmail.com>
2026-01-02 18:19:19 +0530

    change db connection

--- Academy_Backend/src/config/db.js
+++ Academy_Backend/src/config/db.js
@@ -1,14 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Create a pool of connections
+// Create a pool using the DATABASE_URL from Render
 const pool = new Pool({
-    user: process.env.DB_USER,        
-    host: process.env.DB_HOST,        
-    database: process.env.DB_NAME,    
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,        
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for Render PostgreSQL
+    }
 });
-
 
 module.exports = pool;
