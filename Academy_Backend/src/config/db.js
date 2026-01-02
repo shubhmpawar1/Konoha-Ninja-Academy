caa1c059329d98dc1debcebd514ae80664a38368
Shubham Pawar <shubham.ravindra.pawar@gmail.com>
2026-01-02 18:28:20 +0530

    db back

--- Academy_Backend/src/config/db.js
+++ Academy_Backend/src/config/db.js
@@ -7,8 +7,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,        
     database: process.env.DB_NAME,    
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }         
+    port: process.env.DB_PORT,        
 });
 
 
